bc688088018ec4e3ff676336d72cf1cfb1711a2a
United Docks <parkandride.uniteddocks@gmail.com>
2022-09-05 20:27:23 +0400

    retrieving weather from nearesl location

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,3 +1,6 @@
+import 'package:geolocator/geolocator.dart';
+import 'dart:math';
+
 class Weather {
   var locality;
   var iconUrl;
@@ -30,5 +33,69 @@ class Weather {
     maxTemperature = json['MaxTemperature'];
     longitude = json['Longitude'];
     latitude = json['Latitude'];
+  }
+
+  static Weather GetNearestWeatherData(
+      Position position, List<Weather> weatherList) {
+    double longitude = position.longitude;
+    double latitude = position.latitude;
+
+    var possibleWeather = 0;
+    double possibleDistance = 0;
+
+    for (var w = 0; w < weatherList.length; w++) {
+      /*
+      const R = 6371e3; // metres
+      var lat1 = latitude * pi/180; // φ, λ in radians
+      var lat2 = weatherList[w].latitude * pi/180;
+      var latDiff = (latitude-weatherList[w].latitude) * pi/180;
+      var lonDiff = (longitude - weatherList[w].longitude) * pi/180;
+
+      var a = sin(latDiff/2) * sin(latDiff/2) +
+              cos(latDiff) * cos(latDiff) *
+              sin(lonDiff/2) * sin(lonDiff/2);
+      var c = 2 * atan2(sqrt(a), sqrt(1-a));
+
+      var d = R * c; // in metres
+
+*/
+      double distance = haversine(latitude, longitude, weatherList[w].latitude,
+          weatherList[w].longitude);
+
+      if (distance == 0) {
+        possibleWeather = w;
+        possibleDistance = distance;
+
+        break;
+      } else if (w == 0) {
+        possibleWeather = w;
+        possibleDistance = distance;
+      } else if (distance < possibleDistance) {
+        possibleWeather = w;
+        possibleDistance = distance;
+      }
+    }
+
+    print(latitude);
+    print(longitude);
+
+    return weatherList[possibleWeather];
+  }
+
+  static final R = 6372.8; // In kilometers
+
+  static double haversine(double lat1, lon1, lat2, lon2) {
+    double dLat = _toRadians(lat2 - lat1);
+    double dLon = _toRadians(lon2 - lon1);
+    lat1 = _toRadians(lat1);
+    lat2 = _toRadians(lat2);
+    double a =
+        pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(lat1) * cos(lat2);
+    double c = 2 * asin(sqrt(a));
+    return R * c;
+  }
+
+  static double _toRadians(double degree) {
+    return degree * pi / 180;
   }
 }
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4,15 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:myt_weatherapp/api/weatherapi.dart';
 import 'package:myt_weatherapp/components/constants.dart';
 import 'package:myt_weatherapp/components/locator.dart';
+import 'package:myt_weatherapp/models/weather.dart';
 
 class Dashboard extends StatelessWidget {
   var client = WeatherApi();
   var weather;
 
   info() async {
-    //var position = await GetPosition();
+    var position = await GetPosition();
     var weatherList = await client.getWeather();
-    weather = weatherList[3];
+    weather = Weather.GetNearestWeatherData(position, weatherList);
     // print(weather.locality);
     return weather;
   }
@@ -21,43 +22,43 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: FutureBuilder(
-      future: info(),
-      builder: ((context, snapshot) {
-        return Container(
-          child: Column(
-            children: [
-              Container(
-                height: size.height * 0.75,
-                width: size.width,
-                padding: EdgeInsets.only(top: 30),
-                margin: EdgeInsets.only(right: 10, left: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  gradient: LinearGradient(
-                      colors: [kAccentColor, kPrimaryColor],
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topCenter,
-                      stops: [0.01, 0.55]),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      '${weather?.locality}',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 39,
-                          fontWeight: FontWeight.w100),
-                    ),
-                    Text(
-                      'Monday, 05 September',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w100),
-                    ),
+      body: FutureBuilder(
+        future: info(),
+        builder: ((context, snapshot) {
+          return Container(
+            child: Column(
+              children: [
+                Container(
+                  height: size.height * 1.0,
+                  width: size.width,
+                  padding: EdgeInsets.only(top: 30),
+                  margin: EdgeInsets.only(right: 10, left: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(35),
+                    gradient: LinearGradient(
+                        colors: [kAccentColor, kPrimaryColor],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topCenter,
+                        stops: [0.01, 0.55]),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        '${weather?.locality}',
+                        style: TextStyle(
+                            color: kPrimaryLightColor,
+                            fontSize: 39,
+                            fontWeight: FontWeight.w100),
+                      ),
+                      Text(
+                        'Monday, 05 September',
+                        style: TextStyle(
+                            color: kPrimaryLightColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w100),
+                      ),
 
-                    /*
+                      /*
                   Image.asset(
                     '',
                     width: size.width * 0.4,
@@ -65,68 +66,71 @@ class Dashboard extends StatelessWidget {
 
                   */
 
-                    Text(
-                      '19°',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 39,
-                          fontWeight: FontWeight.w100),
-                    ),
-                    Text(
-                      'Partly cloudy ',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w100),
-                    ),
-                  ],
+                      Text(
+                        '${weather?.maxTemperature}°',
+                        style: TextStyle(
+                            color: kPrimaryLightColor,
+                            fontSize: 39,
+                            fontWeight: FontWeight.w100),
+                      ),
+                      Text(
+                        '${weather?.name}',
+                        style: TextStyle(
+                            color: kPrimaryLightColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w100),
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              child: Text(
+                                'Home',
+                                style: TextStyle(
+                                    color: kPrimaryLightColor,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ),
+                            Container(
+                              child: Text(
+                                'Alerts ',
+                                style: TextStyle(
+                                    color: kPrimaryLightColor,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ),
+                            Container(
+                              child: Text(
+                                'Emergenct Info ',
+                                style: TextStyle(
+                                    color: kPrimaryLightColor,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ),
+                            Container(
+                              child: Text(
+                                'Support ',
+                                style: TextStyle(
+                                    color: kPrimaryLightColor,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    child: Text(
-                      'Home',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w100),
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      'Alerts ',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w100),
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      'Emergenct Info ',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w100),
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      'Support ',
-                      style: TextStyle(
-                          color: kPrimaryLightColor,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w100),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      }),
-    ));
+              ],
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
